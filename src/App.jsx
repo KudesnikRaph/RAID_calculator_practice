c1028c16433e8c246678f0c81048d43cfc3ec1b4
KudesnikRaf <kolesnik.akk@gmail.com>
2025-03-13 20:50:22 +0300

    Add piechart for render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Raid from './components/Raid'
+import { PieChart } from "recharts";
+import PieCharts from "./components/PieChart";
 
 function App() {
   return (
     <div className="raid-container">
       <Raid />
+      <PieCharts />
     </div>
   );
 }
