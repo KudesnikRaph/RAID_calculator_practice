ef3397421eeb0383a055223932697cfb6a1169d7
KudesnikRaf <kolesnik.akk@gmail.com>
2025-03-17 22:29:51 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RAID-calculator-practice/",
+  base: "/RAID_calculator_practice/",
 })
