08e60c5d59d8f60a55c05cb2783093e3930eef79
KudesnikRaf <kolesnik.akk@gmail.com>
2025-03-17 22:17:59 +0300

    Change naming

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/raid-calculator-practice/",
+  base: "/RAID-calculator-practice/",
 })
